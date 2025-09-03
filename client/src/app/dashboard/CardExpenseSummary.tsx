a33a92e034ef0274f276dad9ee82d8c14d022d4d
Daniel E. Londoño <daniel.esloh@gmail.com>
2025-09-03 13:04:23 -0500

    feat(frontend-dashboard): update styles and state, add new summary cards
    - Updated dashboard page, global styles, redux setup, and API config
    - Added new dashboard components: CardExpenseSummary, CardPopularProducts, CardPurchaseSummary, and CardSalesSummary
    - CardExpenseSummary component created but functionality not yet implemented

---
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const CardExpenseSummary = () => {
+  return (
+    <div className='row-span-3 bg-gray-500'>CardExpenseSummary</div>
+  )
+}
+
+export default CardExpenseSummary
